c913f660d319b95d6e9aba11df7c4edfbc267c6a
Uin <3169104566@qq.com>
2022-09-08 08:37:31 +0800

    my first commit for idea to git

--- src/main/java/com/cy/store/StoreApplication.java
+++ src/main/java/com/cy/store/StoreApplication.java
@@ -13,6 +13,8 @@ public class StoreApplication {
     public static void main(String[] args) {
         SpringApplication.run(StoreApplication.class, args);
         System.out.println("hello git");
+        System.out.println("hello master");
+        System.out.println("hello hot-fix");
     }
 
 }
